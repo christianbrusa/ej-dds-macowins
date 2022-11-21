c871c04278e4aa7b1db692306b9c8be56b02131f
christianbrusa <christianbrusa@hotmail.com>
2022-11-21 18:44:45 -0300

    Quito instancias que no se usan

--- test/ej-pdep-macowins.spec.js
+++ test/ej-pdep-macowins.spec.js
@@ -12,12 +12,9 @@ require("should");
 
 //Instancio estados
 const nueva = new Nueva();
-const promocion = new Promocion(1200);
-const liquidacion = new Liquidacion();
 
 //Instancio pagos
 const pagoDePruebaConEfectivo = new Efectivo();
-const pagoDePruebaConTarjeta = new Tarjeta(3, 1.1);
 
 //Instancio prendas
 const camisaFloreada = new Prenda(4350, "Camisa", nueva);
