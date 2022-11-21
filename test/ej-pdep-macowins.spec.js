657b714a3095f7d7a3ec66e241590b99428ba1ee
christianbrusa <christianbrusa@hotmail.com>
2022-11-21 18:37:58 -0300

    Agrego test Venta.montoTotal

--- test/ej-pdep-macowins.spec.js
+++ test/ej-pdep-macowins.spec.js
@@ -22,6 +22,9 @@ const pagoDePruebaConTarjeta = new Tarjeta(3, 1.1);
 //Instancio prendas
 const camisaFloreada = new Prenda(4350, "Camisa", nueva);
 
+//Instancio venta
+const ventaPrueba = new Venta("25-10-2022", [{nombre: camisaFloreada, cantidad: 1}], pagoDePruebaConEfectivo);
+
 describe("Se desea saber el tipo de una prenda", () => {
     it("El de una camisa floreada deberia ser: 'Camisa'", () => {
         camisaFloreada.tipo.should.be.eql("Camisa");
@@ -48,6 +51,22 @@ describe("Se desea saber el precio de venta de una prenda", () => {
         });
         it("El precio de la camisa floreada deberia ser: $2175", () => {
             camisaFloreada.precioFinal().should.be.eql(2175);
+        });
+    });
+});
+
+describe("Se desea saber el monto total de una venta", () => {
+    describe("- Si el pago se hizo en efectivo, no hay modificaciones sobre el total", () => {
+        it("El total de una venta que contiene una camisa floreada nueva deberia ser: $4350", () => {
+            ventaPrueba.montoTotal().should.be.eql(4350);
+        });
+    });
+    describe("- Si el pago se hizo con tarjeta, se le aplica un recargo segun las cuotas seleccionadas (cantidad de cuotas * coeficiente fijo + 0.01 del valor de cada prenda)", () => {
+        beforeEach(function() {
+            ventaPrueba.tipoPago = new Tarjeta(3, 1.1);
+        });
+        it("El total de una venta que contiene una camisa floreada nueva, en tres cuotas y con un coeficiente del 1.1 deberia ser: $8746.8", () => {
+            ventaPrueba.montoTotal().should.be.eql(8746.8);
         });
     });
 });
